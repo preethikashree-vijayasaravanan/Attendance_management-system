379255fc4332fcc61e8a24dcac969fc699339e24
Praveena T <23csec18praveenathirumalai@gmail.com>
2026-07-10 22:38:36 +0530

    changes

--- frontendapp/src/api.js
+++ frontendapp/src/api.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Pointing directly to your newly created live Render API server
-  baseURL: 'https://attendance-management-system-vb0u.onrender.com/api',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+  baseURL: 'https://attendance-management-system-vb0u.onrender.com', // Remove /api
+  headers: { 'Content-Type': 'application/json' }
 });
 
 export default api;
